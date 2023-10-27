4660745c2f95e681c53dafd83a4749a85e95f194
sankalpa-acharya <sankalpaacharya6609@gmail.com>
2023-10-27 19:06:10 +0530

    add: added space files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const homeRoute = require('./routes/appRoute')
 const app = express()
+const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.static('public'));
 app.set('view engine','ejs')
@@ -11,7 +12,7 @@ app.use(express.json())
 app.use('/',homeRoute)
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('starting......')
 })
 
